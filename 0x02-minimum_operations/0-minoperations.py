33a1feada98a48f2ded99bafee33ffbd37b128c6
samueltezera28 <samueltezera28@gmail.com>
2023-01-12 22:02:38 +0300

    0 minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python3
-"""
-Module 0x02-minimum_operations
+"""Module for minOP func
 """
 
 
 def minOperations(n: int) -> int:
-    """
-    Returns fewest number of operations
-    needed to result in exaclty n H
-    characters in the file
-    """
-    counter = 0
+    number_of_char_H = 1
+    number_of_clipboard = 1
+    number_of_operations = 0
 
-    if n <= 1:
-        return counter
+    while(number_of_char_H < n):
+        number_of_operations += 1     # cp first
+        number_of_char_H += number_of_clipboard  # first pasted
+        number_of_operations += 1
+        if (number_of_char_H == n):
+            return number_of_operations
 
-    for i in range(2, n + 1):
-        while (0 == n % i):
-            counter = counter + i
-            n = n / i
-            if n < i:
-                break
-    return counter
+        if(n % number_of_char_H == 0):
+            if(number_of_char_H * 2 == n):
+                number_of_operations += 1
+                number_of_char_H *= 2
+                number_of_operations += 1
+                return number_of_operations
+            else:
+                number_of_operations += 1
+                number_of_char_H += number_of_clipboard  # second pasted
+                number_of_clipboard = number_of_char_H  # second copy inside if
+        else:
+            number_of_char_H += number_of_clipboard
+            number_of_operations += 1
+            number_of_clipboard = number_of_char_H  # second copy
+
+    return number_of_operations
