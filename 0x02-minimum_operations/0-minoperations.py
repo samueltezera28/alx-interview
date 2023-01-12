d5c447ad12b2ac3c16a5859bf2035c1881da0d71
samueltezera28 <samueltezera28@gmail.com>
2023-01-12 22:08:18 +0300

    0 minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,33 +1,24 @@
 #!/usr/bin/python3
-"""Module for minOP func
+"""
+     A method that calculates the fewest number of operations needed to result.
 """
 
 
-def minOperations(n: int) -> int:
-    number_of_char_H = 1
-    number_of_clipboard = 1
-    number_of_operations = 0
+def minOperations(n):
+    """
+        A function that calculates the fewest number of operations needed
+    """
 
-    while(number_of_char_H < n):
-        number_of_operations += 1     # cp first
-        number_of_char_H += number_of_clipboard  # first pasted
-        number_of_operations += 1
-        if (number_of_char_H == n):
-            return number_of_operations
-
-        if(n % number_of_char_H == 0):
-            if(number_of_char_H * 2 == n):
-                number_of_operations += 1
-                number_of_char_H *= 2
-                number_of_operations += 1
-                return number_of_operations
-            else:
-                number_of_operations += 1
-                number_of_char_H += number_of_clipboard  # second pasted
-                number_of_clipboard = number_of_char_H  # second copy inside if
+    present = 1
+    start = 0
+    count = 0
+    while present < n:
+        remainder = n - present
+        if (remainder % present == 0):
+            start = present
+            present += start
+            count += 2
         else:
-            number_of_char_H += number_of_clipboard
-            number_of_operations += 1
-            number_of_clipboard = number_of_char_H  # second copy
-
-    return number_of_operations
+            present += start
+            count += 1
+    return count
